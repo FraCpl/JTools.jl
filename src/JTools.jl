a1eb1ac69b503692b8869cd6c136719bdaef7f1b
FraCpl <fra.capolupo@gmail.com>
2024-03-14 16:23:14 +0100

    added nonlinear least squares routine

--- src/JTools.jl
+++ src/JTools.jl
@@ -13,6 +13,7 @@ export dm2dv, dm2dt, dv2dm, dv2dt, units, convertUnits
 export theme_fra, plotMoon!, axisoff!, plotframe!, transformModel, sensorFovModel, cuboidModel
 export nicholsgrid
 export crossmat, logrange, mag2db, db2mag, unwrap!
+export ls
 include("functions/trapz.jl")
 include("functions/ecdf.jl")
 include("functions/goldenSectionSearch.jl")
@@ -22,5 +23,6 @@ include("functions/rocketEqs.jl")
 include("functions/units.jl")
 include("functions/makieTools.jl")
 include("functions/math.jl")
+include("functions/ls.jl")
 
 end
