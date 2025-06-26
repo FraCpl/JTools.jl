77d518d88120680da60832ef46be819d76244582
FraCpl <fra.capolupo@gmail.com>
2025-06-26 15:33:08 +0200

    ups

--- src/functions/math.jl
+++ src/functions/math.jl
@@ -1,7 +1,7 @@
-crossmat(v) = [0.0 -v[3]  v[2]; v[3] 0.0 -v[1]; -v[2] v[1] 0.0];
-logrange(a, b, length=100) = exp10.(range(a; stop=b, length=length))
+@inline crossmat(v) = [0.0 -v[3]  v[2]; v[3] 0.0 -v[1]; -v[2] v[1] 0.0];
+@inline logrange(a, b, length=100) = exp10.(range(a; stop=b, length=length))
 
-function unwrap!(x, period=2π)
+@inline function unwrap!(x, period=2π)
 	y = convert(eltype(x), period)
 	v = first(x)
 	@inbounds for k = eachindex(x)
@@ -12,7 +12,7 @@ end
 # Robust modulo operation (Matlab-like)
 # modup=false (default): modd(x, x) = 0
 # modup=true: modd(x, x) = x
-function modd(x, y; modup=false)
+@inline function modd(x, y; modup=false)
     if isapprox(rem(x, y, RoundNearest), 0.0; atol=1.49e-8)     # 1.49e-8 = sqrt(eps())
         return modup ? y : 0.0
     end
@@ -20,15 +20,15 @@ function modd(x, y; modup=false)
 end
 
 # This function returns true if x is an integer multiple of y
-isMultiple(x, y) = modd(x, y) == 0.0
+@inline isMultiple(x, y) = modd(x, y) == 0.0
 
-signum(x) = x ≥ 0.0 ? 1.0 : -1.0
+@inline signum(x) = x ≥ 0.0 ? 1.0 : -1.0
 
 # Returns interpolating coefficients C, so that
 # y ≈ C[1] + C[2]*x + C[3]*x^2 + ...
-function polyfit(x, y, n)
+@inline function polyfit(x, y, n)
     A = ones(length(x), n + 1)
-    for i in 1:n
+    @inbounds for i in 1:n
         A[:, i+1] = x.^i
     end
     return A\y
@@ -36,7 +36,7 @@ end
 
 # Evaluate polynomial from coefficients C, so that
 # y = C[1] + C[2]*x + C[3]*x^2 + ...
-function polyval(C, x)
+@inline function polyval(C, x)
     y = 0.0
     for i in eachindex(C)
         y += C[i]*x^(i - 1)
@@ -44,7 +44,7 @@ function polyval(C, x)
     return y
 end
 
-function interp1(x, y, xi)
+@inline function interp1(x, y, xi)
     if xi ≥ x[end]
         return y[end]
     elseif xi < x[1]
