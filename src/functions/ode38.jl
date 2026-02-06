fd8362c1882356c5b951ee766034d54b4ff2a3cc
FraCpl <fra.capolupo@gmail.com>
2026-02-06 15:23:02 +0100

    up

--- src/functions/ode38.jl
+++ src/functions/ode38.jl
@@ -29,3 +29,35 @@ function ode38!(x, f!, x0, tspan, K1, K2, K3, K4, K5; nSteps=100)
     end
     return x
 end
+
+# f!(dx, t, x, p)
+@inline function ode38(f!, x0, tspan, p; nSteps=100, K1=similar(x0), K2=similar(x0), K3=similar(x0), K4=similar(x0), K5=similar(x0))
+    # 3/8 Runge-Kutta Method
+    # http://www.mymathlib.com/diffeq/runge-kutta/runge_kutta_3_8.html
+    x = copy(x0)
+    return ode38!(x, f!, x0, tspan, p, K1, K2, K3, K4, K5; nSteps=nSteps)
+end
+
+@inline function ode38!(x, f!, x0, tspan, p, K1, K2, K3, K4, K5; nSteps=100)
+    # 3/8 Runge-Kutta Method
+    # http://www.mymathlib.com/diffeq/runge-kutta/runge_kutta_3_8.html
+    x .= x0
+    t = tspan[1]
+    h = (tspan[2] - tspan[1])/nSteps
+    for _ in 1:nSteps
+        f!(K1, t, x, p)
+
+        @. K5 = x + h*K1/3
+        f!(K2, t + 1/3*h, K5, p)
+
+        @. K5 = x + h*(-K1/3 + K2)
+        f!(K3, t + 2/3*h, K5, p)
+
+        @. K5 = x + h*(K1 - K2 + K3)
+        f!(K4, t + h, K5, p)
+
+        t += h
+        @. x += h*(K1 + 3K2 + 3K3 + K4)/8
+    end
+    return x
+end
