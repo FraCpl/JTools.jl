aa61c4d33c6204cc5582b9234e0388ff5986b4dd
FraCpl <fra.capolupo@gmail.com>
2026-04-13 17:09:27 +0200

    updated ransac

--- src/functions/ransac.jl
+++ src/functions/ransac.jl
@@ -1,56 +1,66 @@
+# RANSAC (MSAC) algorithm
+#
 # distAll = funDist(idFit) // fit using y[idFit], evaluate distance on entire y
 # Robust Parameter Estimation using RANSAC
 #
-# The 'funDist(idFit)' function shall estimate the problem's parameters using measurements
+# The 'funDist(idFit)' function shall estimate the problem's parameters using data
 # identified by the 'idFit' variable (e.g., y[idFit]), and shall return the distance metric
-# of all measurements (inlcuding those used for the fit). The distance shall be positive, it
-# is recommended to use as distance an error metric such as d = e².
+# of all measurements (inlcuding those not used for the fit). The distance shall be *positive*,
+# it is recommended to use as distance an error metric such as d = e².
 #
-# https://www.cse.iitb.ac.in/~ajitvr/CS763_Spring2017/RobustMethods.pdf
-# https://web.ipac.caltech.edu/staff/fmasci/home/astro_refs/LeastMedianOfSquares.pdf
-#
-# Torr, Zisserman, MLESAC: A new robust estimator with application to estimating image geometry
-# https://www.robots.ox.ac.uk/~vgg/publications/2000/Torr00/torr00.pdf
+# References:
+# [1] https://www.cse.iitb.ac.in/~ajitvr/CS763_Spring2017/RobustMethods.pdf
+# [2] https://web.ipac.caltech.edu/staff/fmasci/home/astro_refs/LeastMedianOfSquares.pdf
+# [3] Torr, Zisserman, MLESAC: A new robust estimator with application to estimating image
+#     geometry, https://www.robots.ox.ac.uk/~vgg/publications/2000/Torr00/torr00.pdf
 #
 # Author: F. Capolupo
 # European Space Agency, 2023
 function ransac(
-    funDist,        # Fit + distance function
-    N,                      # Total number of available measurements/data points
-    Nmin;                   # Minimum number of measurements required to compute a fit
-    maxIter=100,            # Maximum number of iterations
+    funDist,                # Fit + distance function
+    N::Int,                 # Total number of available measurements/data points
+    Nmin::Int;              # Minimum number of measurements required to compute a fit
+    maxIter::Int=100,       # Maximum number of iterations
     threshold=0.1,          # Inliers distance threshold
-    verbose=true,
+    verbose::Bool=true,
 )
 
     # Init parameters and allocations
     Cbest = Inf
-    dist = zeros(N, 1)
-    inliers = BitArray(undef, N)
+    dist = zeros(N)
+    inliers = BitVector(undef, N)
     iFit = zeros(Int, Nmin)
+    nPerm = zeros(Int, N)
 
     # Init iterations
     for _ in 1:maxIter
         # Randomly select Nmin points to be used to fit the model
-        iFit .= randperm(N)[1:Nmin]
+        randperm!(nPerm)
+        @inbounds for i in eachindex(iFit)
+            iFit[i] = nPerm[i]
+        end
 
         # Fit the model using selected Nmin random points and calculate
         # distance on entire measurement population
-        dist .= abs.(funDist(iFit))     # added abs just in case (dumb user)
+        dist .= funDist(iFit)
 
         # Identify inliers
-        C = sum(min.(dist, threshold))
+        C = 0.0
+        @inbounds for i in eachindex(dist)
+            dist[i] = abs(dist[i])          # added abs just in case (dumb user)
+            C += min(dist[i], threshold)    # MSAC (Slide 42 of [1])
+        end
 
         # Update best fit if needed
         if C < Cbest
             Cbest = C
-            inliers .= dist .≤ threshold
+            @inbounds for i in eachindex(dist)
+                inliers[i] = dist[i] ≤ threshold
+            end
         end
     end
 
-    if verbose
-        println("RANSAC: $(sum(.!inliers)) outliers detected")
-    end
+    verbose && println("RANSAC: $(sum(.!inliers)) outliers detected")
 
     return inliers
 end
--- test/testRansac.jl
+++ test/testRansac.jl
@@ -2,18 +2,44 @@ using LinearAlgebra
 using JTools
 using BenchmarkTools
 
-a = randn()
-b = randn()
+function distFun!(dist, x, y, id)        # non allocating....
+    # Fit line parameters with data[id]
+    @assert length(id) == 2
+    i1, i2 = id
+    x1, x2 = x[i1], x[i2]
+    y1, y2 = y[i1], y[i2]
+    invd = 1 / (x1 - x2)
 
-x = collect(range(0, 30, 10))
-y = a*x .+ b
-y[3] *= 100
-on = ones(2)
-A = [x ones(length(x))]
+    a = invd * (y1 - y2)
+    b = invd * (x1 * y2 - x2 * y1)
 
-function distFun(id)
-    a, b = A[id, :]\y[id]
-    return abs.(a*x - y .+ b) ./ sqrt(a*a + 1)  # orthogonal distance to line
+    # Compute distance for all data points
+    dd = 1 / sqrt(a *a + 1)
+    @inbounds for i in eachindex(dist)
+        dist[i] = abs(a * x[i] - y[i] + b) * dd # orthogonal distance to line
+    end
+    return dist
 end
 
-@btime ransac(distFun, length(x), 2; verbose=false)
+function testRansac()
+    a = randn()
+    b = randn()
+
+    x = collect(range(0, 30, 10))
+    y = a*x .+ b
+    y[3] *= 100
+    y[4] += 200
+    dist = zeros(length(x))
+
+    idtest = [2; 3]
+    @btime distFun!($dist, $x, $y, $idtest)
+    f(id) = distFun!(dist, x, y, id)
+
+    Nmin = 2; N = length(x)
+
+    @show ransac(f, N, Nmin; verbose=true)
+
+    @btime ransac($f, $N, $Nmin; verbose=false)
+end
+
+testRansac()
